82896c63369d64e27a05d3fd5bc92a691ec264ea
JarekRES <j.trojanowicz@res.rh.pl>
2022-06-03 11:28:40 +0200

    Initial Commit

---
+++ TestUIKit-CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestUIKit-CoreData
+//
+//  Created by Jaroslaw Trojanowicz on 03/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
